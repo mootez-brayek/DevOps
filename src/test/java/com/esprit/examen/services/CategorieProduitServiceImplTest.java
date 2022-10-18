be2dfa7cbcd174e9b5240cfda8988de4f8f2a36d
Chiheb.ALLOUCHE <chihebeddine.allouche@esprit.tn>
2022-10-18 23:56:43 +0100

    Produit TEST

---
+++ src/test/java/com/esprit/examen/services/CategorieProduitServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.esprit.examen.services;
+
+import com.esprit.examen.entities.CategorieProduit;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategorieProduitServiceImplTest {
+
+    @Test
+    void retrieveAllCategorieProduits() {
+    }
+
+    @Test
+    void addCategorieProduit() {
+      /*  List<CategorieProduit> stocks = categorieProduitService.retrieveAllCategorieProduits();
+        int expected=stocks.size();
+        CategorieProduit cp = new CategorieProduit ("stock test",10,100);
+        CategorieProduit savedCategorieProduit = CategorieProduit.addCategorieProduit(cp);
+
+        ssertEquals(expected+1, categorieProduitService.retrieveAllStocks().size());
+        assertNotNull(savedCategorieProduit.getLibelleStock());
+        stockService.deleteStock(savedStock.getIdStock()); */
+    }
+
+    @Test
+    void deleteCategorieProduit() {
+    }
+
+    @Test
+    void updateCategorieProduit() {
+    }
+
+    @Test
+    void retrieveCategorieProduit() {
+    }
+}
