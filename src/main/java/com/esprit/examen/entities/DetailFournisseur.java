ff2ad47e93277398e2a514b9ea2bafebd876e15e
Mootez brayek <mootez.brayek@esprit.tn>
2022-11-08 13:53:53 +0100

    fixes

--- src/main/java/com/esprit/examen/entities/DetailFournisseur.java
+++ src/main/java/com/esprit/examen/entities/DetailFournisseur.java
@@ -1,9 +1,20 @@
 package com.esprit.examen.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+import javax.persistence.OneToOne;
+import javax.persistence.TemporalType;
+import javax.persistence.Temporal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -13,6 +24,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
 public class DetailFournisseur implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
